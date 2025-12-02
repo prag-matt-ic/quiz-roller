f230ce0438014276e02988826f98c93c95a50de1
Loopspeed Ltd <matthew@loopspeed.co.uk>
2025-12-02 21:02:16 +0000

    Update playerPalette.glsl

--- resources/glsl/playerPalette.glsl
+++ resources/glsl/playerPalette.glsl
@@ -3,11 +3,24 @@
 
 // Player palette
 vec3 samplePlayerPalette(in float t) {
-    vec3 a = vec3(0.5, 0.5, 0.5);
-    vec3 b = vec3(0.5, 0.5, 0.5);
-    vec3 c = vec3(1.0, 1.0, 0.5);
-    vec3 d = vec3(0.80, 0.90, 0.30);
+vec3 a = vec3(0.470, 0.500, 0.500);
+vec3 b = vec3(0.500, 0.500, 0.500);
+vec3 c = vec3(1.000, 1.000, 0.490);
+vec3 d = vec3(0.750, 0.910, 0.300);
     return palette(t, a, b, c, d);
 }
 
 #pragma glslify: export(samplePlayerPalette)
+
+
+//  ORIGINAL 
+//     vec3 a = vec3(0.5, 0.5, 0.5);
+//     vec3 b = vec3(0.5, 0.5, 0.5);
+//     vec3 c = vec3(1.0, 1.0, 0.5);
+//     vec3 d = vec3(0.80, 0.90, 0.30);
+
+// ALTERNATIVE PALETTES
+// vec3 a = vec3(0.470, 0.500, 0.500);
+// vec3 b = vec3(0.500, 0.500, 0.500);
+// vec3 c = vec3(1.000, 1.000, 0.490);
+// vec3 d = vec3(0.750, 0.910, 0.300);
